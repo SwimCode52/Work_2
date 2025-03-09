b17b5b133e174e279abc58518c8b4d4bbdc40dc7
Logan <logan.laberge@gmail.com>
2025-03-09 16:34:14 -0400

    Splits State out

--- CBSA/CBSA_Work.py
+++ CBSA/CBSA_Work.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 
-CBSA_Titles= pd.read_csv('/Users/loganlaberge/Documents/Work/Mailing_List/CBSA_Work/ZIP_CBSA/CBSA_Title.csv', dtype={'CBSA Code':"string"})
+CBSA_Titles= pd.read_csv('/Users/loganlaberge/Work_2/CBSA/CBSA_Title.csv', dtype={'CBSA Code':"string"})
 
 
-ZIP_CBSA = pd.read_csv("/Users/loganlaberge/Documents/Work/Mailing_List/CBSA_Work/ZIP_CBSA/ZIP_CBSA.csv", dtype={'ZIP':"string", "CBSA":"string"})
+ZIP_CBSA = pd.read_csv("/Users/loganlaberge/Work_2/CBSA/ZIP_CBSA.csv", dtype={'ZIP':"string", "CBSA":"string"})
 
 
 CBSA_columns=['CBSA Code', 'CBSA Title']
@@ -27,22 +27,27 @@ Combined_nodup=Combined.drop_duplicates(subset=['ZIP', 'CBSA'])
 
 
 def split_states(df, column_name):
-   # Extract state information (assuming it's always at the end of the string)
-   df['State'] = df[column_name].str.extract(r'([A-Z]{2}(?:-[A-Z]{2})*)$')[0]
+    # Extract state information (assumes states are always at the end)
+    df.loc[:, 'State'] = df[column_name].str.extract(r'([A-Z]{2}(?:-[A-Z]{2})*)$')[0]
+    
+
+    # Split hyphenated states into a list (e.g., "NY-NJ" becomes ["NY", "NJ"])
+    df['State'] = df['State'].str.split('-')
+    
+    # Expand the lists into separate rows
+    df = df.explode('State', ignore_index=True)
+
+    return df
 
 
-   # Split hyphenated states and expand into separate rows
-   df = df.assign(State=df['State'].str.split('-')).explode('State', ignore_index=True)
-
-
-   return df
-
-
-split_states(Combined_nodup, 'CBSA Title')
 
 
 
+Combined_nodup_new=split_states(Combined_nodup, 'CBSA Title')
 
-Combined_nodup.to_csv('ZIP_CBSA_Combined.csv', index=False)
+Combined_nodup_new
+
+
+Combined_nodup_new.to_csv('ZIP_CBSA_Combined.csv', index=False)
 
 
