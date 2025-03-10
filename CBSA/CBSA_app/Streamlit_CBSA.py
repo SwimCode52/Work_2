5918bbdca4a4159f9c01219cf02d49e02d7fb283
Logan <logan.laberge@gmail.com>
2025-03-09 21:50:05 -0400

    First Trial

--- CBSA/CBSA_app/Streamlit_CBSA.py
+++ CBSA/CBSA_app/Streamlit_CBSA.py
@@ -19,7 +19,12 @@ def lookup_zips(location):
 def lookup_everything(location):
    df = CBSA[CBSA.Locale == location]
    zips_list = df['Zip'].tolist()
-   if len(zips_list)
+   if len(zips_list) > 222:
+       first_list=zips_list[:222]
+       everything_else=zips_list[222:]
+       return [first_list, everything_else]
+   else:
+       return [zips_list]
 
 
 lookup_zips('New York-Newark-Jersey City, NY-NJ')
@@ -34,7 +39,35 @@ def landing_page():
          thing = list(snip['Locale'].unique())
          holding_tank.extend(iter(thing))
       if locale_options:= st.multiselect('Select Cities:', holding_tank):
-         
+         for town in locale_options:
+            town_zip=lookup_everything(town)
+            if len(town_zip) == 1:
+               csv_list=""
+               for count,entry in town_zip.enumerate():
+                  if count in [0, len(town_zip) - 1]:
+                     f'{csv_list}{entry}'
+                  else:
+                     f'{csv_list}{entry},'
+               st.markdown(f'{town}')
+               st.text_area(f'{csv_list}')
+            else:
+               csv_list_1=""
+               csv_list_2=""
+               for count,entry in town_zip[0].enumerate():
+                  if count in [0, len(town_zip) - 1]:
+                     csv_list+f'{entry}'
+                  else:
+                     csv_list+f'{entry},'
+               for count,entry in town_zip[1].enumerate():
+                  if count in [0, len(town_zip) - 1]:
+                     csv_list+f'{entry}'
+                  else:
+                     csv_list+f'{entry},'
+               st.markdown(f'{town}')
+               st.markdown('First Filter')
+               st.text_area(f'{csv_list_1}')
+               st.markdown('Second Filter')
+               st.text_area(f'{csv_list_1}')
 
    else:
       st.write("No state selected.")
