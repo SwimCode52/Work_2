e1566991accaa847991460de6db606b45ad0a01b
Logan <logan.laberge@gmail.com>
2025-03-23 16:48:48 -0400

    third filter for long CBSA lists

--- CBSA/CBSA_app/Streamlit_CBSA.py
+++ CBSA/CBSA_app/Streamlit_CBSA.py
@@ -26,7 +26,12 @@ def lookup_everything(location, CBSA):
    if len(zips_list) > 222:
        first_list=zips_list[:222]
        everything_else=zips_list[222:]
-       return [first_list, everything_else]
+       if len(everything_else) > 222:
+          second_list= everything_else[:222]
+          last_list=everything_else[222:]
+          return [first_list, second_list,last_list]
+       else:
+         return [first_list, everything_else]
    else:
        return [zips_list]
 
@@ -71,7 +76,7 @@ def landing_page():
                   st.markdown(f'**{town}**')
                   st.text_area("ZIP Codes:", csv_list)  # Corrected argument
 
-               else:
+               elif len(town_zip)==2:
                   csv_list_1 = ""
                   csv_list_2 = ""
 
@@ -94,8 +99,47 @@ def landing_page():
                   st.markdown(f'**{town}**')
                   st.markdown('**First Filter**')
                   st.text_area("ZIP Codes (First 222):", csv_list_1)
+                  st.divider()
                   st.markdown('**Second Filter**')
                   st.text_area("Remaining ZIP Codes:", csv_list_2)
+               else:
+                  csv_list_1 = ""
+                  csv_list_2 = ""
+                  csv_list_3 = ""
+
+                  for count, entry in enumerate(town_zip[0]):  # First filter
+                     if count == 0:
+                        csv_list_1 += f'{entry},'
+                     elif count == len(town_zip[0]) - 1:
+                        csv_list_1 += f'{entry}'
+                     else:
+                        csv_list_1 += f'{entry},'
+
+                  for count, entry in enumerate(town_zip[1]):  # Second filter
+                     if count == 0:
+                        csv_list_2 += f'{entry},'
+                     elif count == len(town_zip[1]) - 1:
+                        csv_list_2 += f'{entry}'
+                     else:
+                        csv_list_2 += f'{entry},'
+                  for count, entry in enumerate(town_zip[2]):  # Second filter
+                     if count == 0:
+                        csv_list_3 += f'{entry},'
+                     elif count == len(town_zip[2]) - 1:
+                        csv_list_3 += f'{entry}'
+                     else:
+                        csv_list_3 += f'{entry},'
+                  
+
+                  st.markdown(f'**{town}**')
+                  st.markdown('**First Filter**')
+                  st.text_area("ZIP Codes (First 222):", csv_list_1)
+                  st.divider()
+                  st.markdown('**Second Filter**')
+                  st.text_area("Second Filter:", csv_list_2)
+                  st.divider()
+                  st.markdown('**Third Filter**')
+                  st.text_area("Remaining ZIP Codes:", csv_list_3)
 
       else:
          st.write("No state selected.")
