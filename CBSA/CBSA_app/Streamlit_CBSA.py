3c39d2beef1533fd1aca7a89da04d33076520d81
Logan <logan.laberge@gmail.com>
2025-03-23 16:28:22 -0400

    MarketingCloudChanges

--- CBSA/CBSA_app/Streamlit_CBSA.py
+++ CBSA/CBSA_app/Streamlit_CBSA.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-CBSA=pd.read_csv('CBSA/CBSA_app/ZIP_CBSA_Combined.csv', dtype={'ZIP':'string', 'CBSA':'string'})
+Correct_CBSA=pd.read_csv('CBSA/CBSA_app/ZIP_CBSA_Combined.csv', dtype={'ZIP':'string', 'CBSA':'string'})
+MC_CBSA=pd.read_csv('CBSA/CBSA_app/MarketingCloud_Zip.csv', dtype={'ZIP':'string', 'CBSA':'string'})
 
-
+#ForDebugging
+# Correct_CBSA=pd.read_csv('/Users/loganlaberge/Work_2/CBSA/ZIP_CBSA_Combined.csv', dtype={'ZIP':'string', 'CBSA':'string'})
+# MC_CBSA=pd.read_csv('/Users/loganlaberge/Work_2/CBSA/CBSA_app/MarketingCloud_Zip.csv', dtype={'ZIP':'string', 'CBSA':'string'})
 
 
 def lookup_zips(location):
@@ -16,7 +19,7 @@ def lookup_zips(location):
    else:
        return [zips_list]
 
-def lookup_everything(location):
+def lookup_everything(location, CBSA):
    df = CBSA[CBSA.Locale == location]
    df_nodup=df.drop_duplicates(subset='ZIP')
    zips_list = df_nodup['ZIP'].tolist()
@@ -28,60 +31,74 @@ def lookup_everything(location):
        return [zips_list]
 
 
-lookup_zips('New York-Newark-Jersey City, NY-NJ')
+# lookup_zips('New York-Newark-Jersey City, NY-NJ')
 
 def landing_page():
    st.title("CBSA Work Around")
-
-   state_names = list(CBSA['State'].unique())
-   if selected_options := st.multiselect("Select State(s):", sorted(state_names)):
-      holding_tank = []
-
-      for thing in selected_options:
-          snip = CBSA[CBSA.State == thing]
-          thing = list(snip['Locale'].unique())
-          holding_tank.extend(thing)
-
-      if locale_options := st.multiselect('Select Cities:', sorted(holding_tank)):
-         for town in locale_options:
-            town_zip = lookup_everything(town)
-
-            if len(town_zip) == 1:
-               csv_list = "".join(
-                    f"{entry}, " if count < len(town_zip[0]) - 1 else f"{entry}"
-                    for count, entry in enumerate(town_zip[0])
-                )
-               st.markdown(f'**{town}**')
-               st.text_area("ZIP Codes:", csv_list)  # Corrected argument
-
-            else:
-               csv_list_1 = ""
-               csv_list_2 = ""
-
-               for count, entry in enumerate(town_zip[0]):  # First filter
-                  if count == 0:
-                     csv_list_1 += f'{entry},'
-                  elif count == len(town_zip[0]) - 1:
-                     csv_list_1 += f'{entry}'
-                  else:
-                     csv_list_1 += f'{entry},'
-
-               for count, entry in enumerate(town_zip[1]):  # Second filter
-                  if count == 0:
-                     csv_list_2 += f'{entry},'
-                  elif count == len(town_zip[1]) - 1:
-                     csv_list_2 += f'{entry}'
-                  else:
-                     csv_list_2 += f'{entry},'
-
-               st.markdown(f'**{town}**')
-               st.markdown('**First Filter**')
-               st.text_area("ZIP Codes (First 222):", csv_list_1)
-               st.markdown('**Second Filter**')
-               st.text_area("Remaining ZIP Codes:", csv_list_2)
-
+   option = st.selectbox(
+    'Select your Dataset:',
+    ('','HUD CBSA List', 'MarketingCloud List')
+)
+   st.write('You selected:', option)
+   if option == 'HUD CBSA List':
+       CBSA=Correct_CBSA
+       st.write(' This option is the most up to date CBSA Data but **does not match Marketing Cloud and should not be used in financial reporting**')
+   elif option == 'MarketingCloud List':
+       CBSA=MC_CBSA
+       st.write('This selection matches the current mapping for Marketing Cloud.')
    else:
-      st.write("No state selected.")
+      st.write(' Please select option')
+
+   if option in ['HUD CBSA List', 'MarketingCloud List']:
+      state_names = list(CBSA['State'].unique())
+      if selected_options := st.multiselect("Select State(s):", sorted(state_names)):
+         holding_tank = []
+
+         for thing in selected_options:
+            snip = CBSA[CBSA.State == thing]
+            thing = list(snip['Locale'].unique())
+            holding_tank.extend(thing)
+
+         if locale_options := st.multiselect('Select Cities:', sorted(holding_tank)):
+            for town in locale_options:
+               town_zip = lookup_everything(town, CBSA)
+
+               if len(town_zip) == 1:
+                  csv_list = "".join(
+                     f"{entry}, " if count < len(town_zip[0]) - 1 else f"{entry}"
+                     for count, entry in enumerate(town_zip[0])
+                  )
+                  st.markdown(f'**{town}**')
+                  st.text_area("ZIP Codes:", csv_list)  # Corrected argument
+
+               else:
+                  csv_list_1 = ""
+                  csv_list_2 = ""
+
+                  for count, entry in enumerate(town_zip[0]):  # First filter
+                     if count == 0:
+                        csv_list_1 += f'{entry},'
+                     elif count == len(town_zip[0]) - 1:
+                        csv_list_1 += f'{entry}'
+                     else:
+                        csv_list_1 += f'{entry},'
+
+                  for count, entry in enumerate(town_zip[1]):  # Second filter
+                     if count == 0:
+                        csv_list_2 += f'{entry},'
+                     elif count == len(town_zip[1]) - 1:
+                        csv_list_2 += f'{entry}'
+                     else:
+                        csv_list_2 += f'{entry},'
+
+                  st.markdown(f'**{town}**')
+                  st.markdown('**First Filter**')
+                  st.text_area("ZIP Codes (First 222):", csv_list_1)
+                  st.markdown('**Second Filter**')
+                  st.text_area("Remaining ZIP Codes:", csv_list_2)
+
+      else:
+         st.write("No state selected.")
 
 if __name__ == "__main__":
     landing_page()
